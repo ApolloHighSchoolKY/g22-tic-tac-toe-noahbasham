dd7c1bf5be56fceab45da5bb2bf61401b1268005
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-02-18 22:23:49 +0000

    commit

--- Lab24d.java
+++ Lab24d.java
@@ -13,9 +13,21 @@ public class Lab24d
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
+		//Read the number at the top
+		int iterations = Integer.valueOf(file.nextLine());
+		
+		//Loop through the different games
+		for(int i = 0; i< iterations; i++)
+		{
+			//Next game is a string
+			String game = file.nextLine();
+			System.out.println(game);
+			TicTacToe play = new TicTacToe(game);
+			System.out.println(play);
+			System.out.println(play.getWinner() + "\n");
+		}
 
-
-
+		file.close();
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -12,44 +12,51 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
+		mat = new char[3][3];
 
 	}
 
 	public TicTacToe(String game)
 	{
-
-
-
+		mat = new char[3][3];
+		int pos = 0; //index of the char in the string that needs to be placed in the array
+		for(int r = 0; r<3; r++)
+		{
+			for(int c = 0; c<3; c++)
+			{
+				mat[r][c] = game.charAt(pos); //set the spot in the array
+				pos++; //move to the next character
+			}
+		}
 
 	}
 
 	public String getWinner()
 	{
-
-
-
-
-
-
-
-
-
-
-
-
-		return "";
+		//check for horizontal wins
+		for(int r = 0; r<3; r++)
+		{
+			if(mat[r][0] == mat[r][1] && mat[r][1] == mat[r][2])
+				return "" + mat[r][0] + " wins horizontally!";
+		}
+		//check for vertical wins
+		for(int c = 0; c<3; c++)
+		{
+			if(mat[0][c] == mat[1][c] && mat[1][c] == mat[2][c])
+				return "" + mat[0][c] + " wins vertically!";
+		}
+		//check for diagonal wins - two ifs
+		if(mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2])
+			return "" + mat[1][1] + " wins diagonally!"; 
+		else if(mat[2][0] == mat[1][1] && mat[1][1] == mat[0][2])
+			return "" + mat[1][1] + " wins diagonally!"; 
+		else
+		return "cat's game - no winner!";
 	}
 
 	public String toString()
 	{
-		String output="";
-
-
-
-
-
-
+		String output="" + mat[0][0] + " " + mat[0][1] + " " + mat[0][2] + "\n" + mat[1][0] + " " + mat[1][1] + " " + mat[1][2] + "\n" + mat[2][0] + " " + mat[2][1] + " " + mat[2][2];
 		return output+"\n\n";
 	}
 
